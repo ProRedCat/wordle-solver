0059b0ccd75185fd5a8c7f362c163ef71a1e01f9
Reilly Oldham <reilly.oldham@gmail.com>
2022-06-27 19:57:19 +1200

    Refactored code into class
    WordleSolver class has been created which has the responsibilities for solving the wordle game and processing inputs

--- wordleSolver.py
+++ wordleSolver.py
@@ -3,83 +3,140 @@ import numpy as np
 
 from typing import Dict, Set, List
 
-def evaluateWord(word: str, answer: str) -> List[int]:
-    wordUsedLetters = np.full(len(word), False)
-    answerUsedLetters = np.full(len(word), False)
+class WordleSolver:
+    def __init__(self, words: List[str]) -> None:
+        self.guessedWords = list()
 
-    evaluation = np.full(len(word), 0)
+        self.greyLetters = set()
+        self.yellowLetters = dict()
+        self.greenLetters = dict()
 
-    for i in range(len(word)):
-        if(word[i] == answer[i]):
-            evaluation[i] = 2
-            wordUsedLetters[i] = True
-            answerUsedLetters[i] = True
+        self.usedLetters = set()
 
-    for i in range(len(word)):
-        if(wordUsedLetters[i] == False):
-            for j in range(len(answer)):
-                if(answerUsedLetters[j] == False and word[i] == answer[j]):
-                    evaluation[i] = 1
-                    answerUsedLetters[j] = True
-                    wordUsedLetters[i] = True     
-                    break  
+        self.maxLetters = dict()
+        self.minLetters = dict()
 
-    return evaluation
+        self.wordsList = list(words)
+    
+    def evaluateWord(self, word: str, answer: str) -> List[int]:
+        wordUsedLetters = np.full(len(word), False)
+        answerUsedLetters = np.full(len(word), False)
 
-def validateWord(greyLetters: Set[str], yellowLetters: Dict[str, List[int]], greenLetters: Dict[str, List[int]], maxLetters: Dict[str, int], minLetters: Dict[str, int], word: str) -> bool:        
-    for letter in maxLetters:
-        if(word.count(letter) > maxLetters[letter]):
-            return False
+        evaluation = np.full(len(word), 0)
 
-    for letter in minLetters:
-        if(word.count(letter) < minLetters[letter]):
-            return False
+        for i in range(len(word)):
+            if(word[i] == answer[i]):
+                evaluation[i] = 2
+                wordUsedLetters[i] = True
+                answerUsedLetters[i] = True
 
-    for letter in greyLetters:
-        if(letter in word):
-            return False
+        for i in range(len(word)):
+            if(wordUsedLetters[i] == False):
+                for j in range(len(answer)):
+                    if(answerUsedLetters[j] == False and word[i] == answer[j]):
+                        evaluation[i] = 1
+                        answerUsedLetters[j] = True
+                        wordUsedLetters[i] = True     
+                        break  
 
-    for containedLetter in yellowLetters:
-        if(containedLetter in word):
-            positions = yellowLetters[containedLetter]
-            for position in positions:
-                if(word[position] == containedLetter):
-                    return False
-        else:
-            return False 
+        return evaluation
 
-    for confirmedLetter in greenLetters:
-        if(confirmedLetter in word):
-            positions = greenLetters[confirmedLetter]
-            for position in positions:
-                if(word[position] != confirmedLetter):
-                    return False
-        else:
-            return False
+    def validateWord(self, word: str) -> bool:        
+        for letter in self.maxLetters:
+            if(word.count(letter) > self.maxLetters[letter]):
+                return False
 
-    return True
+        for letter in self.minLetters:
+            if(word.count(letter) < self.minLetters[letter]):
+                return False
 
-def eliminateWords(greyLetters: Set[str], yellowLetters: Dict[str, List[int]], greenLetters: Dict[str, List[int]], maxLetters: Dict[str, int], minLetters: Dict[str, int], wordsList) -> List[str]:
-    validWords = []
+        for letter in self.greyLetters:
+            if(letter in word):
+                return False
 
-    for word in wordsList:
-        if(validateWord(greyLetters, yellowLetters, greenLetters, maxLetters, minLetters, word)):
-            validWords.append(word)
+        for containedLetter in self.yellowLetters:
+            if(containedLetter in word):
+                positions = self.yellowLetters[containedLetter]
+                for position in positions:
+                    if(word[position] == containedLetter):
+                        return False
+            else:
+                return False 
 
-    return validWords
+        for confirmedLetter in self.greenLetters:
+            if(confirmedLetter in word):
+                positions = self.greenLetters[confirmedLetter]
+                for position in positions:
+                    if(word[position] != confirmedLetter):
+                        return False
+            else:
+                return False
 
-def findBestWord(guessedWords: List[str], wordsList: List[str]) -> str:
-    wordCount = 0
-    wordEvaluation = dict()
-    for word in wordsList:
-        if(word not in guessedWords):
-            wordEvaluation[word] = 0
-            for answer in wordsList:
-                wordEvaluation[word] += sum(evaluateWord(word, answer))
+        return True
 
-        wordCount += 1
-        
-    return max(wordEvaluation, key=wordEvaluation.get)
+    def eliminateWords(self) -> List[str]:
+        validWords = list()
+
+        for word in self.wordsList:
+            if(self.validateWord(word)):
+                validWords.append(word)
+
+        self.wordsList = validWords
+
+    def findBestWord(self) -> str:
+        wordCount = 0
+        wordEvaluation = dict()
+        for word in self.wordsList:
+            if(word not in self.guessedWords):
+                wordEvaluation[word] = 0
+                for answer in self.wordsList:
+                    wordEvaluation[word] += sum(self.evaluateWord(word, answer))
+
+            wordCount += 1
+
+        if(len(wordEvaluation) == 0):
+            return ""    
+            
+        return max(wordEvaluation, key=wordEvaluation.get)
+
+    def processWord(self, word: str, result: List[str]):
+        self.guessedWords.append(word)
+
+        letterCounts = dict()
+        for letter in word:
+            letterCounts[letter] = word.count(letter)
+
+        for letter in letterCounts:
+            if(letterCounts[letter] > 1):
+                foundMax = False
+                for i in range(len(result)):
+                    if(word[i] == letter and result[i] == "0"):
+                        letterCount = 0
+                        for j in range(len(result)):
+                            if(word[j] == letter and (result[j] == "1" or result[j] == "2")):
+                                letterCount += 1
+                        self.maxLetters[letter] = letterCount
+                        foundMax = True
+                if(foundMax == False):
+                    self.minLetters[letter] = letterCounts[letter]
+
+        for i in range(len(result)):
+            if(result[i] == "0"):
+                if(word[i] not in self.maxLetters):
+                    self.greyLetters.add(word[i])
+            elif(result[i] == "1"):
+                if(word[i] in self.yellowLetters):
+                    self.yellowLetters[word[i]].add(i)
+                else:
+                    self.yellowLetters[word[i]] = {i} 
+            elif(result[i] == "2"):
+                if(word[i] in self.greenLetters):
+                    self.greenLetters[word[i]].add(i)
+                else:
+                    self.greenLetters[word[i]] = {i}
+            self.usedLetters.add(word[i])
+
+        self.eliminateWords()
 
 words = []
 
@@ -89,17 +146,7 @@ with open("five-letter-words.txt", 'r') as f:
 for i in range(len(words)):
     words[i] = words[i].replace("\n", "").lower()
 
-guessedWords = []
-greyLetters = set()
-yellowLetters = dict()
-greenLetters = dict()
-
-usedLetters = set()
-
-maxLetters = dict()
-minLetters = dict()
-
-wordsList = list(words)
+wordleSolver = WordleSolver(words)
 
 numOfGuesses = 0
 
@@ -107,66 +154,27 @@ while(True):
     guessedWord = str(input("What word was guessed? ")).lower()
     wordResult = str(input("What was the result? ")).split()
 
-    guessedWords.append(guessedWord)
     numOfGuesses += 1
 
     if(wordResult.count("2") == 5):
         break
 
-    letterCounts = dict()
-    for letter in guessedWord:
-        letterCounts[letter] = guessedWord.count(letter)
+    wordleSolver.processWord(guessedWord, wordResult)
 
-    for letter in letterCounts:
-        if(letterCounts[letter] > 1):
-            foundMax = False
-            for i in range(len(wordResult)):
-                if(guessedWord[i] == letter and wordResult[i] == "0"):
-                    letterCount = 0
-                    for j in range(len(wordResult)):
-                        if(guessedWord[j] == letter and (wordResult[j] == "1" or wordResult[j] == "2")):
-                            letterCount += 1
-                    maxLetters[letter] = letterCount
-                    foundMax = True
-            if(foundMax == False):
-                minLetters[letter] = letterCounts[letter]
-
-    for i in range(len(wordResult)):
-        if(wordResult[i] == "0"):
-            if(guessedWord[i] not in maxLetters):
-                greyLetters.add(guessedWord[i])
-        elif(wordResult[i] == "1"):
-            if(guessedWord[i] in yellowLetters):
-                yellowLetters[guessedWord[i]].add(i)
-            else:
-                yellowLetters[guessedWord[i]] = {i} 
-        elif(wordResult[i] == "2"):
-            if(guessedWord[i] in greenLetters):
-                greenLetters[guessedWord[i]].add(i)
-            else:
-                greenLetters[guessedWord[i]] = {i}
-        usedLetters.add(guessedWord[i])
-    
-    # print(greyLetters)
-    # print(yellowLetters)
-    # print(greenLetters)
-    # print(maxLetters)
-    
-    wordsList = eliminateWords(greyLetters, yellowLetters, greenLetters, maxLetters, minLetters, wordsList)
 
     print("Remaining words:")
-    for word in wordsList:
+    for word in wordleSolver.wordsList:
         print("- " + word)
     print()
-    if(len(wordsList) == 1):
-        print("The answer is: " + wordsList[0])
+    if(len(wordleSolver.wordsList) == 1):
+        print("The answer is: " + wordleSolver.wordsList[0])
     else:
         print("Best guess: ")
-        print("- " + findBestWord(guessedWords, wordsList))
+        print("- " + wordleSolver.findBestWord())
         print()
 
 print()
 print("Number of guesses " + str(numOfGuesses))
 print("Guesses: ")
-for word in guessedWords:
+for word in wordleSolver.guessedWords:
     print("- " + word)
